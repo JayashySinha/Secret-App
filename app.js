55805ed388a898425ca42b28d3622d5cb4243130
Jayashy Sinha <jayashysinha@gmail.com>
2021-05-24 14:56:12 +0530

    hashing implemented

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express =require ("express");
 const bodyParser = require ("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const bcrypt = require('bcrypt');
+const saltRounds =10 ;
 
 const app = express();
 
@@ -12,15 +13,13 @@ app.use(express.static("public"));
 app.set('view engine', "ejs");
 app.use (bodyParser.urlencoded({extended:true}));
 
-mongoose.connect("mongodb://localhost:27017/useriddb", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb://localhost:27017/secretsdb", { useNewUrlParser: true, useUnifiedTopology: true })
 
 const userSchema = new mongoose.Schema( {
     email: String,
     password: String
 });
 
-
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields : ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -37,23 +36,27 @@ app.get("/",function(req,res){
 })
 
 app.post("/register",function(req,res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("secrets");
+            }
+        });
     });
-    newUser.save(function(err){
-        if(err){
-            console.log(err);
-        }else{
-            res.render("secrets");
-        }
-    });
+
 });
 
 
 app.post("/login",function(req,res){
     const username = req.body.username
-    const password =req.body.password
+    const password = req.body.password
 
     User.findOne({email: username}, function(err,foundUser){
         if (err){
@@ -61,17 +64,21 @@ app.post("/login",function(req,res){
         }
         else{
             if(foundUser){
-            if(foundUser.password===password){
-                res.render("secrets")
-            }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result===true){
+                        res.render("secrets");
+                    }
+                    else{
+                        console.log("Incorrect Password");
+                    }
+                })}
+           
+            
             else{
-                console.log("Incorrect Password")
+            console.log("User not found")
             }
         }
-        else{
-            console.log("User not found")
-        }
-    }
+    
     })
 })
 
