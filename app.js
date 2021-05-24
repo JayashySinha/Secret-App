4f30da5e3c1b503572a24266d2f8d31aa40c2901
Jayashy Sinha <jayashysinha@gmail.com>
2021-05-24 20:16:45 +0530

    added a procfile

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express =require ("express");
 const bodyParser = require ("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require('bcrypt');
-const saltRounds =10 ;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 
 const app = express();
 
@@ -13,15 +15,30 @@ app.use(express.static("public"));
 app.set('view engine', "ejs");
 app.use (bodyParser.urlencoded({extended:true}));
 
-mongoose.connect("mongodb://localhost:27017/secretsdb", { useNewUrlParser: true, useUnifiedTopology: true })
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+    //cookie: { secure: true }
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
+mongoose.connect("mongodb+srv://admin-jayashy:jayashy1@cluster0.hydym.mongodb.net/secretsdb", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set("useCreateIndex",true);
 
 const userSchema = new mongoose.Schema( {
     email: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose)
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy())
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.get("/login",function(req,res){
     res.render("login");
@@ -35,57 +52,62 @@ app.get("/",function(req,res){
     res.render("home");
 })
 
+app.get("/secrets",function(req,res){
+    if (req.isAuthenticated()){
+    res.render("secrets");}
+    else{
+        res.redirect("/login")
+    }
+})
+
+app.get("/logout",function(req,res){
+    req.logout();
+    res.redirect("/")
+})
+
 app.post("/register",function(req,res){
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save(function(err){
-            if(err){
-                console.log(err);
-            }else{
-                res.render("secrets");
-            }
-        });
-    });
+    User.register({username: req.body.username}, req.body.password, function(err,user){
+        if (err){
+            console.log(err);
+            res.redirect("/register");
+        }
+        else{
+            passport.authenticate("local"(req, res, function(){
+                res.redirect("/secrets")
 
+            }))
+        }
+    })
 });
 
 
 app.post("/login",function(req,res){
-    const username = req.body.username
-    const password = req.body.password
+    const user =new User ({
+    username : req.body.username,
+    password : req.body.password
+});
+ req.login(user,function(err){
+     if (err){
+         console.log(err);
+     }
+     else{
+        passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
 
-    User.findOne({email: username}, function(err,foundUser){
-        if (err){
-            console.log(err);
-        }
-        else{
-            if(foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if (result===true){
-                        res.render("secrets");
-                    }
-                    else{
-                        console.log("Incorrect Password");
-                    }
-                })}
-           
-            
-            else{
-            console.log("User not found")
-            }
-        }
-    
-    })
-})
+        })
+     }
+ });
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
 
-
-app.listen(3000,function(){
+app.listen(port,function(){
     console.log("Server started on port 3000.")
 })
 
